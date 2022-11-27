061ecd8e2245d4b68fd9b002bd04c2dc444aa7ef
Dmitry <vaipens666@gmail.com>
2022-11-27 04:29:15 +0300

    courses slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,3 +19,37 @@ btnLangs.forEach((el) => {
       menuLangs.forEach((m) => m.classList.toggle('langs-list-active'));
    });
 });
+
+// slider courses
+$(function () {
+   $('.courses__slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: $('.slider-courses-btn-left'),
+      nextArrow: $('.slider-courses-btn-right'),
+      dots: true,
+      responsive: [
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1
+            }
+         },
+         {
+            breakpoint: 400,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
+         }
+      ]
+   });
+});
+
+let x = document.querySelectorAll('ul.slick-dots li button');
+
+x.forEach(el => {
+   el.textContent = "";
+   console.log(el);
+});
